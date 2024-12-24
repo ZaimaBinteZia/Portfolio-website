441f926102b99613107c974368071e091e1f1ab1
Zaimabintezia <Zaimazia68@gmail.com>
2024-12-24 22:29:23 +0600

    animal website done

---
+++ script.js
@@ -0,0 +1,11 @@
+function getOwlInfo() {
+
+    document.getElementById("owl_info").innerHTML = "Owls are nocturnal birds of prey belonging to the order Strigiformes";
+
+}
+
+function getPandaInfo() {
+
+    document.getElementById("panda_info").innerHTML = "Pandas are large, herbivorous mammals native to China, known for their distinct black and white fur.";
+
+}
